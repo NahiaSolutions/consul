fd7873e74a81a6eb2dbdfaa72b476946ab977942
Manu <nahia.desarrollo@gmail.com>
2019-01-08 09:49:32 -0500

    fix migration

--- db/migrate/20181009200441_fix_houses_columns_names.rb
+++ db/migrate/20181009200441_fix_houses_columns_names.rb
@@ -1,6 +1,6 @@
 class FixHousesColumnsNames < ActiveRecord::Migration
   def change
-    rename_column :houses, :id_administration, :administration_id
+    rename_column :houses, :id_administration, :zonal_administration_id
     rename_column :house_news, :id_house, :house_id
     rename_column :workshops, :id_house, :house_id
     rename_column :workshop_users, :id_workshop, :workshop_id
