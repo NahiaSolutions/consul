4553a1a677214ef3449b70ef890a410130f4cfb2
Manu <nahia.desarrollo@gmail.com>
2019-01-25 12:29:10 -0500

    add numbers to house name and zonal administration name

--- app/models/custom/house.rb
+++ app/models/custom/house.rb
@@ -8,7 +8,7 @@ class House < ActiveRecord::Base
     accepts_nested_attributes_for   :map_location
     include Imageable
 
-    validates :name, :presence => true, format: {with: /\A^[a-zA-Zñáéíóú\s]+$\Z/i}
+    validates :name, :presence => true, format: {with: /\A^[a-zA-Z0-9ñáéíóú\s]+$\Z/i}
     validates :address, :presence => true, format: {with: /\A^[a-zA-Zñáéíóú0-9.\-#,\s]+$\Z/i}
     validates :schedule, :presence => true, format: {with: /\A^[a-zA-Z0-9\s\/:.\-]+$\Z/i}
     validates :phone, :presence => true, format: {with: /\A^[0-9\/]+$\Z/i}
--- app/models/custom/zonal_administration.rb
+++ app/models/custom/zonal_administration.rb
@@ -2,5 +2,5 @@ class ZonalAdministration < ActiveRecord::Base
     has_many :houses, :dependent => :destroy
 
     #Validaciones
-    validates :name, :presence => true, format: {with: /\A^[a-zA-Zñáéíóú\s]+$\Z/i}
+    validates :name, :presence => true, format: {with: /\A^[a-zA-Z0-9ñáéíóú\s]+$\Z/i}
 end
